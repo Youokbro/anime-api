7788edc9fed2dd32c651ab395a395fd39f83f51b
Youokbro <ahaantadi@gmail.com>
2026-06-14 22:43:48 +0530

    feat: add Hianime provider and optimize search speed by reordering providers

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ app.use((req, res, next) => {
   next()
 })
 
-const PROVIDERS = ['AnimeSaturn', 'AnimeUnity', 'AnimeKai', 'KickAssAnime', 'AnimePahe']
+const PROVIDERS = ['Hianime', 'AnimePahe', 'AnimeSaturn', 'AnimeUnity', 'AnimeKai', 'KickAssAnime']
 const WORKER_FETCH = 'https://anim-proxy.ahaantadi.workers.dev/fetch'
 const REF_MAP = {
   AnimeSaturn: 'https://www.animesaturn.cx',
