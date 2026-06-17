54b877fd5a1f8bb676bb117ef24f74e0e331d21a
Youokbro <ahaantadi@gmail.com>
2026-06-17 22:45:10 +0530

    Fix CORS preflight - add Allow-Headers for content-type POST requests

--- api/index.js
+++ api/index.js
@@ -7,7 +7,10 @@ const app = express()
 app.use(express.json())
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
   res.header('Content-Type', 'application/json')
+  if (req.method === 'OPTIONS') return res.status(204).end()
   next()
 })
 
